bca9eecdcc2b7432c312dff4fc4e4b515fd1894a
INTERUPT13 <97730158+INTERUPT13@users.noreply.github.com>
2022-09-28 05:42:57 -0500

    Update initrd-config.nix

--- initrd-config.nix
+++ initrd-config.nix
@@ -5,10 +5,10 @@
       # networking.firewall.allowedUDPPorts = [ ... ];
       # Or disable the firewall altogether.
 
-      #boot.initrd.network = {
+      boot.initrd.network = {
+        post.commands = "/bin/sh";
 
-
-      #};
+      };
 
       # changeme (zfs needs this option) via head -c 8 /etc/machine-id
       networking.hostId = "5b7b7b41";

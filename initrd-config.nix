57e2a267140e261bce48ba4738130c0ea057c342
some body <bakami9@airmail.cc>
2022-09-28 05:28:32 -0500

    up

--- initrd-config.nix
+++ initrd-config.nix
@@ -5,21 +5,10 @@
       # networking.firewall.allowedUDPPorts = [ ... ];
       # Or disable the firewall altogether.
 
-      boot.initrd.network = {
+      #boot.initrd.network = {
 
-        postCommands = ''
-          cat <<EOF > /root/.profile
-          if pgrep -x "zfs" > /dev/null
-          then
-            zfs load-key -a
-            killall zfs
-          else
-            echo "zfs not running -- maybe the pool is taking some time to load for some unforseen reason."
-          fi
-          EOF
-        '';
 
-      };
+      #};
 
       # changeme (zfs needs this option) via head -c 8 /etc/machine-id
       networking.hostId = "5b7b7b41";
